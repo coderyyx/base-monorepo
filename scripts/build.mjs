ff2758ef97d011a5c4eb50fc616308b31a123617
elvan <coderyyx@gmail.com>
2025-07-15 15:38:00 +0800

    chore: 添加 Changesets 配置文件和 README，更新构建脚本以输出发布计划

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -7,6 +7,7 @@ try {
   // 获取需要发布的包
   const releasePlan = await getReleasePlan(process.cwd());
 
+  console.log('releasePlan:', releasePlan.releases);
   // 过滤掉 private 包
   const pkgs = [];
   for (const release of releasePlan.releases || []) {
