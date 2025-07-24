9f09d4e3eb5125f5ada2903d94b8efbceaddd92f
elvan <coderyyx@gmail.com>
2025-07-24 15:54:48 +0800

    feat: 添加 utils-js 示例库，包含基本构建配置和工具函数

---
+++ examples/utils-js/.lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx,css,scss,md,json,yml}': 'prettier --write',
+};
---
+++ examples/utils-js/rollup.config.mjs
@@ -0,0 +1,60 @@
+import { readFileSync } from 'fs';
+
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+const isESModule = process.env.BUILD_FORMAT === 'esm';
+
+// 获取外部依赖
+const external = Object.keys({ ...pkg.dependencies, ...pkg.peerDependencies });
+
+// Babel 配置
+const babelConfig = {
+  allowAllFormats: true,
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: false,
+        modules: false,
+      },
+    ],
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        useESModules: isESModule,
+        version: '7.9.0',
+      },
+    ],
+  ],
+};
+
+// 输出配置
+const outputConfig = isESModule
+  ? {
+      format: 'es',
+      entryFileNames: '[name].js',
+      dir: 'dist/esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    }
+  : {
+      format: 'cjs',
+      entryFileNames: '[name].js',
+      exports: 'named',
+      dir: 'dist/lib',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    };
+
+export default {
+  input: 'src/index.js',
+  output: outputConfig,
+  external,
+  plugins: [nodeResolve(), commonjs(), getBabelOutputPlugin(babelConfig)],
+};
